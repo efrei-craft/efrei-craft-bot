d60673c45c196759532ea13f2615dab3b777e7da
DocSystem <42613513+DocSystem@users.noreply.github.com>
2022-10-10 10:24:44 +0200

    Fix DB

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,11 +1,11 @@
 const { ContextMenuCommandBuilder, EmbedBuilder } = require('@discordjs/builders');
 const mariadb = require("mariadb");
 const pool = mariadb.createPool({
-    host: '127.0.0.1',
-    port: '3307',
-    user: 'discord',
-    password: 'EfreiCraftBot',
-    database: 'efreicraft',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     connectionLimit: 5
 });
 
