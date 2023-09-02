b0963ccd72753c9152b69fbb380c8f8e0a1f3baf
DocSystem <42613513+DocSystem@users.noreply.github.com>
2023-09-02 04:24:00 +0200

    Add new New member interaction

--- commands/startmemberize.js
+++ commands/startmemberize.js
@@ -7,6 +7,16 @@ module.exports = {
     async execute(interaction) {
         const user = interaction.targetMember;
         user.roles.add("1028938423253356544");
+        // Remove all previously messages from bot to user in private channel
+        // Send private message to member
+        await user.send(`Bonjour <@${user.id}>,\n
+Merci d'avoir rejoint One Panthéon et Efrei Craft ! Tu peux désormais devenir **Membre du serveur** !
+Pour cela, il te reste **une dernière étape** à effectuer, rendez-vous dans le salon <#1028938731337547786> et **suis les instructions**.\n
+À bientôt sur le serveur !
+L'équipe d'Efrei Craft`);/*.then(async msg => {
+            const messages = await msg.channel.messages.fetch();
+            messages.filter(m => m.author.id === interaction.client.user.id).forEach(m => m.delete());
+        })*/
         interaction.reply({content: "L'utilisateur <@" + user.id + "> a été converti en nouveau membre avec succès !", ephemeral: true});
     }
 }
--- events/roleUpdate.js
+++ events/roleUpdate.js
@@ -17,19 +17,21 @@ module.exports = {
             }
         }
         // POLES
-        const playerUuid = (await animus.getPlayerFromDiscordId(newMember.id)).uuid;
-        if (playerUuid !== null) {
-            const groupMap = require("../config.json").discord_roles_id;
-            const ranks = [];
-            for (const role of newMember.roles.cache.values()) {
-                for (const roleName in groupMap) {
-                    if (groupMap[roleName] === role.id) {
-                        ranks.push(roleName);
+        try {
+            const playerUuid = (await animus.getPlayerFromDiscordId(newMember.id)).uuid;
+            if (playerUuid !== null) {
+                const groupMap = require("../config.json").discord_roles_id;
+                const ranks = [];
+                for (const role of newMember.roles.cache.values()) {
+                    for (const roleName in groupMap) {
+                        if (groupMap[roleName] === role.id) {
+                            ranks.push(roleName);
+                        }
                     }
                 }
+                await animus.updatePlayerGroups(playerUuid, ranks);
+                console.log("Updated permissions groups of " + newMember.user.username + " (" + newMember.id + ") to " + ranks.join(", ") + ".");
             }
-            await animus.updatePlayerGroups(playerUuid, ranks);
-            console.log("Updated permissions groups of " + newMember.user.username + " (" + newMember.id + ") to " + ranks.join(", ") + ".");
-        }
+        } catch {}
     }
 }
