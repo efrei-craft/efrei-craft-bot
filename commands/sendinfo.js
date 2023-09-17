3332ab4f0098ab0abb81645e7154a0f637a22cdd
DocSystem <42613513+DocSystem@users.noreply.github.com>
2023-09-17 22:54:15 +0200

    Update rules

--- commands/sendinfo.js
+++ commands/sendinfo.js
@@ -40,7 +40,7 @@ const channelInfos = {
                 },
                 {
                     name: "\u200B",
-                    value: "> :seven: Il est **interdit** d'utiliser toute technique de **triche** sur les **serveurs Minecraft**, quelle qu'elle soit (AutoClick, X-Ray, Baritone, etc...). Si vous avez un doute sur l'utilisation d'un mod, contactez un membre du bureau."
+                    value: "> :seven: Il est **interdit** d'utiliser toute technique de **triche** sur les **serveurs Minecraft**, quelle qu'elle soit (AutoClick, X-Ray, Baritone, etc...). Si vous avez un doute sur l'utilisation d'un mod, contactez un membre du bureau. Veuillez noter que le bureau se réserve la discretion de définir ce qui confère un avantage injuste ou non."
                 },
                 {
                     name: "\u200B",
